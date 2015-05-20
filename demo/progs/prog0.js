830bf1e640ef0a1651433b0e6f89bd043d5d8d90
Russel Simmons <symstym@mac.com>
2015-05-20 14:37:06 -0700

    factor out demo progs, clean up root lexEnv

---
+++ demo/progs/prog0.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function main(runtime, startTime, argSlots, outputSlot, baseTopoOrder, lexEnv) {
+  if (argSlots.length !== 0) {
+    throw new Error('called with wrong number of arguments');
+  }
+
+  // add application for final output (slot already created)
+  var $_unappOut = runtime.addApplication(startTime, lexEnv.add, [lexEnv.inputA, lexEnv.inputB], outputSlot, baseTopoOrder+'1');
+
+  // create and return deactivator closure. it needs to undo any applications
+  return function() {
+    $_unappOut();
+  };
+}
+
+module.exports = {
+  main: main,
+};
