4c5a130dd1901b598be68fa4789c1158a6a18dcd
Russel Simmons <symstym@mac.com>
2015-06-01 21:17:14 -0700

    Cleanup/simplification of compiler internals
    Removed creation of parallel node/"ref" structure, just modify
    AST in place. Simplified name resolution.

--- compiler/index.js
+++ compiler/index.js
@@ -2,14 +2,6 @@
 
 var parser = require('./parser.js');
 
-var NODE_OP = 1;
-var NODE_LEXENV = 2;
-var NODE_LITERAL = 3;
-
-var REF_UNRESOLVED = 1;
-var REF_RESOLVING = 2;
-var REF_RESOLVED = 3;
-
 function indentFuncExpr(code) {
   var lines = code.trim().split('\n');
   for (var j = 1; j < lines.length; j++) {
@@ -18,41 +10,8 @@ function indentFuncExpr(code) {
   return lines.join('\n');
 }
 
-// takes AST expression node and returns a 'ref' object
-function createNodesRefs(exprNode) {
-  if (exprNode.type === 'op') {
-    var argRefs = [];
-    for (var i = 0; i < exprNode.args.length; i++) {
-      argRefs.push(createNodesRefs(exprNode.args[i]));
-    }
-    return {
-      state: REF_RESOLVED,
-      node: {
-        type: NODE_OP,
-        op: exprNode.op,
-        argRefs: argRefs,
-      },
-    };
-  } else if (exprNode.type === 'varIdent') {
-    return {
-      state: REF_UNRESOLVED,
-      ident: exprNode.ident,
-    };
-  } else if (exprNode.type === 'literal') {
-    return {
-      state: REF_RESOLVED,
-      node: {
-        type: NODE_LITERAL,
-        kind: exprNode.kind,
-        value: exprNode.value,
-      },
-    };
-  } else {
-    throw new Error('Unexpected node type found in AST');
-  }
-}
-
 function compileFunction(paramNames, bodyParts) {
+  // derive "set" of parameter names for easy lookup
   var paramNamesSet = {};
   for (var i = 0; i < paramNames.length; i++) {
     paramNamesSet[paramNames[i]] = null;
@@ -74,111 +33,98 @@ function compileFunction(paramNames, bodyParts) {
     throw new Error('No yield clause found in function body');
   }
 
-  // build explicit graph of two types of nodes:
-  // - nodes corresponding to streams in the lexical environment
-  // - nodes corresponding to local "register" streams we will create
+  var yieldExpr = yieldObj.expr;
 
-  // create node tree for yield expression
-  var outputNode = createNodesRefs(yieldObj.expr);
-
-  // create node trees for each binding
-  var locallyBoundNames = {}; // names bound in this function body mapped to refs (parameters are not considered bindings)
+  var localBindingExprs = {}; // mapping of names bound in this function to their expressions
   for (var i = 0; i < bodyParts.length; i++) {
     var bp = bodyParts[i];
     if (bp.type === 'binding') {
       if (paramNamesSet.hasOwnProperty(bp.ident)) {
         throw new Error('Can\'t bind name to same name as a parameter');
       }
-      if (locallyBoundNames.hasOwnProperty(bp.ident)) {
+      if (localBindingExprs.hasOwnProperty(bp.ident)) {
         throw new Error('Same name bound more than once');
       }
-      locallyBoundNames[bp.ident] = createNodesRefs(bp.expr);
+      localBindingExprs[bp.ident] = bp.expr;
     }
   }
 
-  var usedLexEnvNames = {}; // mapping from names referred to in lexical environment to their nodes (NOT refs)
+  // Name resolution
+  // Return value is a (possibly) new node ref that caller should use in place of argument node ref
+  // We'll detect if there are any "name loops", like "a = b; b = a".
+  var RES_IN_PROGRESS = 1;
+  var RES_COMPLETE = 2;
+  function resolveNamesRecursive(node) {
+    if (node.type === 'op') {
+      for (var i = 0; i < node.args.length; i++) {
+        node.args[i] = resolveNamesRecursive(node.args[i]);
+      }
+      return node;
+    } else if (node.type === 'varIdent') {
+      if (node.resState === undefined) {
+        if (localBindingExprs.hasOwnProperty(node.ident)) {
+          node.resState = RES_IN_PROGRESS;
 
-  // resolve identifier to a node, and make sure any downstream refs are also resolved. returns node
-  function resolveIdentRecursive(ident) {
-    if (locallyBoundNames.hasOwnProperty(ident)) {
-      resolveRefRecursive(locallyBoundNames[ident]);
-      return locallyBoundNames[ident].node;
-    } else {
-      // assume name must refer to something defined in lexical environment
-      if (usedLexEnvNames.hasOwnProperty(ident)) {
-        // resolve to already-created lexenv node
-        return usedLexEnvNames[ident];
+          var n = localBindingExprs[node.ident];
+          n = resolveNamesRecursive(n);
+
+          node.resNode = n;
+          node.resState = RES_COMPLETE;
+
+          return n;
+        } else {
+          // TODO: check if node.ident is actually in lexical environment(!)
+
+          // change the type of this node to lexEnv. 'ident' property stays unchanged
+          node.type = 'lexEnv';
+          return node;
+        }
+      } else if (node.resState === RES_IN_PROGRESS) {
+        throw new Error('Circular bindings');
+      } else if (node.resState === RES_COMPLETE) {
+        return node.resNode;
       } else {
-        // TODO: check that this is a legitimate reference, i.e. ident is actually in lexical environment
-
-        var newNode = {
-          type: NODE_LEXENV,
-          ident: ident,
-        };
-        usedLexEnvNames[ident] = newNode;
-        return newNode;
+        throw new Error('Invalid resState');
       }
-    }
-  }
-
-  // resolve given ref, and recursively resolve any refs found in downstream nodes. returns nothing
-  function resolveRefRecursive(ref) {
-    if (ref.state === REF_RESOLVED) {
-      // do nothing
-    } else if (ref.state === REF_RESOLVING) {
-      throw new Error('Circular binding');
-    } else if (ref.state === REF_UNRESOLVED) {
-      ref.state = REF_RESOLVING;
-      ref.node = resolveIdentRecursive(ref.ident);
-      ref.state = REF_RESOLVED;
+    } else if (node.type === 'literal') {
+      // nothing to do
+      return node;
+    } else if (node.type === 'lexEnv') {
+      // nothing to do
+      return node;
     } else {
-      throw new Error('Invalid ref state');
-    }
-
-    // now that node is ensured to be resolved, recursively make sure everything downstream from it is resolved
-    // TODO: it seems like there's duplicate work happening here. we could put flag on nodes to say that anything
-    //  downstream of it was already resolved?
-    if (ref.node.type === NODE_OP) {
-      for (var i = 0; i < ref.node.argRefs.length; i++) {
-        resolveRefRecursive(ref.node.argRefs[i]);
-      }
-    } else if (ref.node.type === NODE_LEXENV) {
-      // nothing to resolve
-    } else if (ref.node.type === NODE_LITERAL) {
-      // nothing to resolve
-    } else {
-      throw new Error('Invalid node type');
+      throw new Error('Unexpected node type');
     }
   }
 
-  // resolve references to either lexical environment or local bindings
-  resolveRefRecursive(outputNode);
-  for (var k in locallyBoundNames) {
-    resolveRefRecursive(locallyBoundNames[k]);
+  // Do name resolution from all expression roots
+  yieldExpr = resolveNamesRecursive(yieldExpr);
+  for (var k in localBindingExprs) {
+    localBindingExprs[k] = resolveNamesRecursive(localBindingExprs[k]);
   }
 
-  // DFS from outputNode to get toposorted list of nodes
-  var STATE_ENTERED = 1; // node has been entered in traversal, but not yet added to ordering
-  var STATE_ADDED = 2; // node has been added to ordering, and is "done"
+  // Topological sorting to determine computation/update order
+  var TOPOSTATE_ENTERED = 1; // node has been entered in traversal, but not yet added to ordering
+  var TOPOSTATE_ADDED = 2; // node has been added to ordering, and is "done"
   var sortedNodes = [];
   function toposortVisit(node) {
-    if (node.state === STATE_ENTERED) {
-      throw new Error('Cycle in binding/reference graph, can\'t toposort');
-    } else if (node.state === STATE_ADDED) {
+    if (node.topoState === TOPOSTATE_ENTERED) {
+      throw new Error('Cycle in computation graph, can\'t toposort');
+    } else if (node.topoState === TOPOSTATE_ADDED) {
       // already taken care of
       return;
     }
 
-    node.state = STATE_ENTERED;
+    node.topoState = TOPOSTATE_ENTERED;
 
     // visit any nodes this node depends on
-    if (node.type === NODE_OP) {
-      for (var i = 0; i < node.argRefs.length; i++) {
-        toposortVisit(node.argRefs[i].node);
+    if (node.type === 'op') {
+      for (var i = 0; i < node.args.length; i++) {
+        toposortVisit(node.args[i]);
       }
-    } else if (node.type === NODE_LEXENV) {
+    } else if (node.type === 'lexEnv') {
       // nothing to do since leaf
-    } else if (node.type === NODE_LITERAL) {
+    } else if (node.type === 'literal') {
       // nothing to do since leaf
     } else {
       throw new Error('Unexpected node type found during toposort');
@@ -186,16 +132,18 @@ function compileFunction(paramNames, bodyParts) {
 
     // finally, add this node to sort order and update its state
     sortedNodes.push(node);
-    node.state = STATE_ADDED;
-  }
-  toposortVisit(outputNode.node);
-  // NOTE: nodes not already added to sortedNodes are not needed to compute output,
-  //  but we might have inner functions that refer to names defined in this scope
-  for (var k in locallyBoundNames) {
-    toposortVisit(locallyBoundNames[k].node);
+    node.topoState = TOPOSTATE_ADDED;
   }
 
-  // store the topographic sort order numbers on nodes themselves
+  // Traverse from all expression roots, building sortedNodes array
+  toposortVisit(yieldExpr);
+  // NOTE: Nodes not already added to sortedNodes are not needed to compute output,
+  //  but we might have inner functions that refer to names defined in this scope.
+  for (var k in localBindingExprs) {
+    toposortVisit(localBindingExprs[k]);
+  }
+
+  // Store the topographic sort order numbers on nodes themselves.
   for (var i = 0; i < sortedNodes.length; i++) {
     sortedNodes[i].topoOrder = i;
   }
@@ -212,9 +160,9 @@ function compileFunction(paramNames, bodyParts) {
   }
 
   function getNodeStreamExpr(node) {
-    if ((node.type === NODE_OP) || (node.type === NODE_LITERAL)) {
+    if ((node.type === 'op') || (node.type === 'literal')) {
       return '$_reg' + node.topoOrder;
-    } else if (node.type === NODE_LEXENV) {
+    } else if (node.type === 'lexEnv') {
       return '$_var' + node.ident;
     } else {
       throw new Error('Unexpected node type found in tree');
@@ -225,10 +173,10 @@ function compileFunction(paramNames, bodyParts) {
   var deactivatorCalls = [];
   for (var i = 0; i < sortedNodes.length; i++) {
     var node = sortedNodes[i];
-    if (node.type === NODE_OP) {
+    if (node.type === 'op') {
       var argStreamExprs = [];
-      for (var j = 0; j < node.argRefs.length; j++) {
-        argStreamExprs.push(getNodeStreamExpr(node.argRefs[j].node));
+      for (var j = 0; j < node.args.length; j++) {
+        argStreamExprs.push(getNodeStreamExpr(node.args[j]));
       }
 
       var opFuncName = 'runtime.opFuncs.' + node.op;
@@ -237,9 +185,9 @@ function compileFunction(paramNames, bodyParts) {
       codeFragments.push('  var $_act' + node.topoOrder + ' = ' + opFuncName + '(runtime, startTime, [' + argStreamExprs.join(', ') + '], baseTopoOrder+\'' + node.topoOrder + '\', null); var $_reg' + node.topoOrder + ' = $_act' + node.topoOrder + '.outputStream\n');
 
       deactivatorCalls.push('$_act' + node.topoOrder + '.deactivator()');
-    } else if (node.type === NODE_LEXENV) {
+    } else if (node.type === 'lexEnv') {
       // do nothing
-    } else if (node.type === NODE_LITERAL) {
+    } else if (node.type === 'literal') {
       var litValueExpr;
       if (node.kind === 'string') {
         // TODO: we might want to call a proper repr()-style escape on the value, but it should only be safe characters anyways
@@ -260,8 +208,8 @@ function compileFunction(paramNames, bodyParts) {
   }
 
   // add vars for bound names
-  for (var k in locallyBoundNames) {
-    codeFragments.push('  var $_var' + k + ' = $_reg' + locallyBoundNames[k].node.topoOrder + ';\n');
+  for (var k in localBindingExprs) {
+    codeFragments.push('  var $_var' + k + ' = $_reg' + localBindingExprs[k].topoOrder + ';\n');
   }
 
   // I don't think these actually need to be reversed for things to work correctly,
