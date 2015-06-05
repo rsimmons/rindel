bd4d711b19577a88439c79f342a9f4721775b4d9
Russel Simmons <symstym@mac.com>
2015-06-05 12:18:55 -0700

    improved script to build gh-pages
    This change is mostly so that we don't need to have the bundle.js
    build result committed to master.

---
+++ build-push-gh-pages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+REPO="git@github.com:rsimmons/temporal-splendor.git"
+
+# courtesy of http://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
+BUILDTMP=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+
+echo "Building in $BUILDTMP ..."
+
+echo "Cloning from github ..."
+git clone --depth 1 $REPO $BUILDTMP/src
+
+SRC_SHA="$(cd $BUILDTMP/src && git rev-parse HEAD)"
+
+echo "Doing build ..."
+(
+cd $BUILDTMP/src
+cd compiler
+npm install
+cd ../runtime
+npm install
+cd ../demo
+npm install
+npm run build-prod
+)
+
+echo "Cloning gh-pages from github ..."
+git clone --depth 1 --branch gh-pages $REPO $BUILDTMP/dst
+
+echo "Copying from build dir to gh-pages clone ..."
+rsync -a --delete --exclude '.git' $BUILDTMP/src/demo/public/ $BUILDTMP/dst
+
+echo "Commiting and pushing to origin gh-pages ..."
+(
+cd $BUILDTMP/dst
+git add --all
+git commit -m "build of $SRC_SHA"
+git push origin gh-pages:gh-pages
+)
+
+echo "Cleaning up temp dir ..."
+rm -rf $BUILDTMP
+
+echo "Done."
--- pushToPages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-git subtree split --prefix demo/public -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
